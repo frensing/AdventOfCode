1e471ec7ac7fb90ab8e1f9f846558fe8bb040ab7
frensing <frensing@mail.upb.de>
2020-12-10 16:16:56 +0200

    day03 - more pretty

--- day03/day03.py
+++ day03/day03.py
@@ -1,39 +1,25 @@
 with open('day03_input.txt', 'r') as input:
     lines = [line.strip() for line in input]
 
-hits_1 = 0
-hits_2 = 0
-hits_3 = 0
-hits_4 = 0
-hits_5 = 0
 
-pos_x_1 = 0  # Slope 3,1
-pos_x_2 = 0  # Slope 1,1
-pos_x_3 = 0  # Slope 5,1
-pos_x_4 = 0  # Slope 7,1
-pos_x_5 = 0  # Slope 1,2
-pos_y_5 = 0
+def get_hits(slope):
+    pos = [0, 0]
+    hits = 0
+    for line in lines:
+        if line[pos[0]] == '#' and pos[1] == 0:
+            hits += 1
+        if pos[1] == 0:
+            pos[0] = (pos[0] + slope[0]) % len(line)
+        pos[1] = (pos[1] + 1) % slope[1]
+    return hits
 
-for line in lines:
-    if line[pos_x_1] == '#':
-        hits_1 += 1
-    if line[pos_x_2] == '#':
-        hits_2 += 1
-    if line[pos_x_3] == '#':
-        hits_3 += 1
-    if line[pos_x_4] == '#':
-        hits_4 += 1
-    if line[pos_x_5] == '#' and pos_y_5 == 0:
-        hits_5 += 1
 
-    pos_x_1 = (pos_x_1 + 3) % len(line)
-    pos_x_2 = (pos_x_2 + 1) % len(line)
-    pos_x_3 = (pos_x_3 + 5) % len(line)
-    pos_x_4 = (pos_x_4 + 7) % len(line)
-    if pos_y_5 == 0:
-        pos_x_5 = (pos_x_5 + 1) % len(line)
-    pos_y_5 = (pos_y_5 + 1) % 2
+slopes = [[3, 1], [1, 1], [5, 1], [7, 1], [1, 2]]
 
-print("You hit %s trees." % hits_1)
+result_2 = 1
+for slope in slopes:
+    result_2 *= get_hits(slope)
 
-print("Product of hits: %s" % (hits_1 * hits_2 * hits_3 * hits_4 * hits_5,))
+print("You hit %s trees." % get_hits(slopes[0]))
+
+print("Product of hits: %s" % result_2)
