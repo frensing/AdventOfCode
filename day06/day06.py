7f9d5e53dd11d1154130b8aef07c34f11bb1d700
frensing <frensing@mail.upb.de>
2020-12-11 13:54:33 +0200

    day06 - Part 2

--- day06/day06.py
+++ day06/day06.py
@@ -1,14 +1,34 @@
+def get_counts(group, members):
+    c_1 = len(group.keys())
+    c_2 = 0
+    for v in group.values():
+        if v == members:
+            c_2 += 1
+    return (c_1, c_2)
+
+
 with open('input.txt', 'r') as input:
     group = {}
-    counts = 0
+    counts_1 = 0
+    counts_2 = 0
+    members = 0
     for line in input:
         line = line.strip()
         if line == '':
-            counts += len(group.keys())
+            counts = get_counts(group, members)
+            counts_1 += counts[0]
+            counts_2 += counts[1]
+            members = 0
             group = {}
             continue
         for c in line:
-            group[c] = None
-    counts += len(group.keys())
+            if c not in group:
+                group[c] = 0
+            group[c] += 1
+        members += 1
+    counts = get_counts(group, members)
+    counts_1 += counts[0]
+    counts_2 += counts[1]
 
-    print("Part 1:", counts)
+    print("Part 1:", counts_1)
+    print("Part 2:", counts_2)
