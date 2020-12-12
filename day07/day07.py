1b7777cf6d7ad333e20c7f47b90f1dbd597238f1
frensing <frensing@mail.upb.de>
2020-12-12 02:31:57 +0200

    day07 - part 2

--- day07/day07.py
+++ day07/day07.py
@@ -1,6 +1,7 @@
 import re
 
-rules = {}
+rules_up = {}
+rules_down = {}
 rule_pattern = re.compile(r'^(.+) bags contain (.+)')
 bag_pattern = re.compile(r'(\d+) (.+?) bags?')
 
@@ -8,6 +9,8 @@ with open('input.txt', 'r') as input:
     for line in input:
         m_rule = rule_pattern.match(line.strip())
         color = m_rule.group(1)
+        if color not in rules_down:
+            rules_down[color] = {}
         bags = [elm.strip() for elm in m_rule.group(2).split(',')]
         for bag in bags:
             m_bag = bag_pattern.match(bag)
@@ -15,20 +18,28 @@ with open('input.txt', 'r') as input:
                 continue
             count = m_bag.group(1)
             bag_color = m_bag.group(2)
-            if bag_color not in rules:
-                rules[bag_color] = {}
-            rules[bag_color][color] = count
+            if bag_color not in rules_up:
+                rules_up[bag_color] = {}
+            rules_up[bag_color][color] = int(count)
+            rules_down[color][bag_color] = int(count)
 
 
     def explore_color(color, explored):
-        for k in rules[color].keys():
+        for k in rules_up[color].keys():
             if k not in explored:
                 explored[k] = None
-                if k in rules:
+                if k in rules_up:
                     explored = explore_color(k, explored)
         return explored
 
 
-    outer_bags = explore_color('shiny gold', {}).keys()
+    def count_down(color):
+        count = 0
+        for k, v in rules_down[color].items():
+            count += v + v * count_down(k)
+        return count
 
-    print("Part 1:", len(outer_bags))
+
+    print("Part 1:", len(explore_color('shiny gold', {}).keys()))
+
+    print("Part 2:", count_down('shiny gold'))
