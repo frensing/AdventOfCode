3f16da09260bbbc423dae499f6dddac42c154b3b
frensing <frensing@mail.upb.de>
2020-12-13 19:33:52 +0200

    Day 10 - part 2
    (thank you Henning)

--- day10/day10.py
+++ day10/day10.py
@@ -1,10 +1,10 @@
 numbers = []
 
 with open('input.txt', 'r') as input:
-    for line in input:
-        numbers.append(int(line.strip()))
+    numbers = [int(line.strip()) for line in input]
 
 numbers.sort()
+max_val = numbers[-1] + 3
 
 prev_num = 0
 jolt_1 = 0
@@ -17,3 +17,31 @@ for num in numbers:
     prev_num = num
 
 print("Part 1:", jolt_1 * jolt_3)
+
+
+def get_combi_count(adapters):
+    adapters.sort()
+    combi_count = [1, ]
+    if 1 in adapters:
+        combi_count.append(1)
+        if 2 in adapters:
+            combi_count.append(2)
+        else:
+            combi_count.append(0)
+    else:
+        combi_count.append(0)
+        if 2 in adapters:
+            combi_count.append(1)
+        else:
+            combi_count.append(0)
+
+    for i in range(3, adapters[-1] + 1):
+        if i in adapters:
+            combi_count.append(combi_count[i - 1] + combi_count[i - 2] + combi_count[i - 3])
+        else:
+            combi_count.append(0)
+
+    return combi_count[-1]
+
+
+print("Part 2:", get_combi_count(numbers))
