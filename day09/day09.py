ed5b367b607beba1044f39ace96d36a5f18ad3ec
frensing <frensing@mail.upb.de>
2020-12-13 15:30:09 +0200

    day09 - part 2

--- day09/day09.py
+++ day09/day09.py
@@ -1,3 +1,4 @@
+numbers = []
 preamble_len = 25
 preamble = []
 
@@ -10,14 +11,33 @@ def check_number(number):
     return False
 
 
-with open('input.txt', 'r') as input:
-    for i in range(preamble_len):
-        preamble.append(int(next(input).strip()))
+def find_sum(number):
+    index_num = numbers.index(number)
+    for i in range(index_num):
+        sum = numbers[i]
+        for j in range(i + 1, index_num):
+            sum += numbers[j]
+            if sum == number:
+                used_num = numbers[i:j + 1].copy()
+                used_num.sort()
+                return used_num[0] + used_num[-1]
+            if sum > number:
+                break
+    return False
 
+
+with open('input.txt', 'r') as input:
     for line in input:
-        number = int(line.strip())
-        if not check_number(number):
-            print("Part 1:", number)
-            break
-        preamble.pop(0)
-        preamble.append(number)
+        numbers.append(int(line.strip()))
+
+for i in range(preamble_len):
+    preamble.append(numbers[i])
+
+for num in numbers[preamble_len:]:
+    if not check_number(num):
+        print("Part 1:", num)
+        break
+    preamble.pop(0)
+    preamble.append(num)
+
+print("Part 2:", find_sum(num))
