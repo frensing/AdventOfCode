c0a0b14e18d56ea2c1730c99dfca18575deb2672
frensing <refaren+it@mailbox.org>
2021-12-15 00:03:55 +0100

    day14 part1

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -1,33 +1,50 @@
+import time
+
 with open('input.txt', 'r') as inp:
-    template = list(inp.readline().strip())
+    template = inp.readline().strip()
 
     inp.readline()
 
-    rules = {line.strip().split(' -> ')[0]: line.strip().split(' -> ')[1] for line in inp}
+    rules = {}
+    for line in inp:
+        key, ins = line.strip().split(' -> ')
+        rules[key] = [key[0] + ins, ins + key[1]]
 
 rounds = 10
-for j in range(rounds):
-    i = 0
-    while i < len(template) - 1:
-        pair = ''.join(template[i:i + 2])
-        if pair in rules:
-            template.insert(i + 1, rules[pair])
-            i += 1
 
-        i += 1
-
-    print(j)
 
 char_count = {}
-for c in template:
-    if c not in char_count:
-        char_count[c] = 0
-    char_count[c] += 1
+for key in rules:
+    for c in key:
+        if c not in char_count:
+            char_count[c] = 0
+
+
+def dfs(depth, node):
+    if depth == 0:
+        char_count[node[0]] += 1
+        return
+
+    dfs(depth - 1, rules[node][0])
+    dfs(depth - 1, rules[node][1])
+
+
+for i in range(len(template) - 1):
+    start = time.time()
+    dfs(rounds, template[i:i + 2])
+    print(i)
+    print(time.time() - start)
+    print()
+
+char_count[template[-1]] += 1
+
+
+print()
+for c in char_count:
+    print(c, char_count[c])
 
 char_count = sorted(char_count.items(), key=lambda x: x[1])
 most_common = int(char_count[-1][1])
 least_common = int(char_count[0][1])
 
 print("Part 1:", most_common - least_common)
-
-
---
+++ 2021/day14/test.py
@@ -0,0 +1,4 @@
+x = 10
+for i in range(40-24):
+    x *= 2
+    print(24 + i + 1, x / 60 / 60, 'min')
